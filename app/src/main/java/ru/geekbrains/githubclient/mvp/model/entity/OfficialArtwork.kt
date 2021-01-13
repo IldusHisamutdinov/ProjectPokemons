973bacb0387339fd3f976520a9085c0dcb9d2dff
IldusHisamutdinov <63410157+IldusHisamutdinov@users.noreply.github.com>
2021-01-13 12:04:22 +0500

    Delete OfficialArtwork.kt
    лишнее

--- app/src/main/java/ru/geekbrains/githubclient/mvp/model/entity/OfficialArtwork.kt
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.githubclient.mvp.model.entity
-
-import com.google.gson.annotations.Expose
-
-data class OfficialArtwork(
-        @Expose
-        val front_default: String? = null
-)
